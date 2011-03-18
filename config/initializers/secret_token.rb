302e18bbab50c7248892387703028b798c9c3f89
Paul Dacus <pauldacus@g5searchmarketing.com>
2011-03-18 10:08:11 -0700

    G5-CMS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+G5cms::Application.config.secret_token = '916ead5f821159d8bac83d3840c2b8e500554c230e836c220b513518a5f6b15bc81d3a53352b3c9a6bda3053a8f6322ece6717daea9fe9519f3eadb56a21e296'
